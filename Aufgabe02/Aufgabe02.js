f03a6e779e013bcb2d5aa673da120a2af8966152
dennisbenz <dennis.vimon@web.de>
2019-04-11 14:26:47 +0200

    Aufgabe 2 verbessert

--- Aufgabe02/Aufgabe02.js
+++ Aufgabe02/Aufgabe02.js
@@ -7,13 +7,17 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 */
 var MauSpiel;
 (function (MauSpiel) {
-    document.addEventListener("DomContenLoaded", Eingabe);
+    document.addEventListener("DOMContentLoaded", Eingabe);
     let sign = ["He7", "He8", "He9", "He1", "HeA", "HeB", "HeK", "HeD", "Ka7", "Ka8", "Ka9", "Ka1", "KaA", "KaB", "KaK", "KaD", "Kr7", "Kr8", "Kr9", "Kr1", "KrA", "KrB", "KrK", "KrD", "Pi7", "Pi8", "Pi9", "Pi1", "PiA", "PiB", "PiK", "PiD"];
-    let handCardsArray;
+    let handCardsArray = [];
     //Zufallszahl 
-    function Eingabe() {
-        var kartenanzahl = prompt("Kartenanzahl");
+    function kartenanzahlnumber() {
+        let kartenanzahl = prompt("Kartenanzahl");
         let n = parseInt(kartenanzahl);
+        return n;
+    }
+    function Eingabe() {
+        let n = kartenanzahlnumber();
         for (let anz = n; anz > 0; anz--) {
             let r = Math.floor(Math.random() * (sign.length - 1));
             let cardtype = sign[r].substring(0, 2);
@@ -21,21 +25,47 @@ var MauSpiel;
             console.log(cardtype);
             console.log(cardnumber);
             cardgenerate(cardtype, cardnumber);
-        }
-        function cardgenerate(_cardtype, _cardnumber) {
-            let div = document.createElement("div");
-            document.getElementById("Handkarten").appendChild(div);
-            if (_cardtype == "Pi" || _cardtype == "Kr") {
-                console.log(_cardtype + " if");
-                div.classList.add("schwarz");
-            }
-            else {
-                div.classList.add("rot");
-            }
-            div.classList.add("Handkarten");
-            div.innerHTML = "<p>" + _cardnumber + "</p>";
+            handCardsArray.push(sign[r]);
+            sign.splice(r, 1);
         }
     }
-    document.addEventListener("DOMContentLoaded", Eingabe);
+    function cardgenerate(_cardtype, _cardnumber) {
+        let div = document.createElement("div");
+        document.getElementById("Handkarten").appendChild(div);
+        if (_cardtype == "Pi" || _cardtype == "Kr") {
+            console.log(_cardtype + " if");
+            div.classList.add("schwarz");
+        }
+        else {
+            div.classList.add("rot");
+        }
+        div.classList.add("Handkarten");
+        div.innerHTML = "<p>" + cardshow(_cardtype) + _cardnumber + "</p>";
+    }
+    function cardshow(_cardtype) {
+        let emblem;
+        switch (_cardtype) {
+            case "Kr": {
+                emblem = "♣";
+                break;
+            }
+            case "Pi": {
+                emblem = "♠";
+                break;
+            }
+            case "He": {
+                emblem = "♥";
+                break;
+            }
+            case "Ka": {
+                emblem = "♦";
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+        return emblem;
+    }
 })(MauSpiel || (MauSpiel = {}));
 //# sourceMappingURL=Aufgabe02.js.map
--- Aufgabe02/Aufgabe02.ts
+++ Aufgabe02/Aufgabe02.ts
@@ -6,15 +6,23 @@ Datum: 04.04.2019
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
 namespace MauSpiel {
-    document.addEventListener("DomContenLoaded", Eingabe);
+    document.addEventListener("DOMContentLoaded", Eingabe);
 
     let sign: string[] = ["He7", "He8", "He9", "He1", "HeA", "HeB", "HeK", "HeD", "Ka7", "Ka8", "Ka9", "Ka1", "KaA", "KaB", "KaK", "KaD", "Kr7", "Kr8", "Kr9", "Kr1", "KrA", "KrB", "KrK", "KrD", "Pi7", "Pi8", "Pi9", "Pi1", "PiA", "PiB", "PiK", "PiD"];
-    let handCardsArray: string[];
+    let handCardsArray: string[] = [];
+
+
     //Zufallszahl 
+    function kartenanzahlnumber(): number {
+        let kartenanzahl: string = prompt("Kartenanzahl");
+        let n: number = parseInt(kartenanzahl);
+
+        return n;
+    }
 
     function Eingabe(): void {
-        var kartenanzahl: string = prompt("Kartenanzahl");
-        let n: number = parseInt(kartenanzahl);
+        let n: number = kartenanzahlnumber();
+
 
         for (let anz: number = n; anz > 0; anz--) {
             let r: number = Math.floor(Math.random() * (sign.length - 1));
@@ -24,24 +32,63 @@ namespace MauSpiel {
             console.log(cardtype);
             console.log(cardnumber);
             cardgenerate(cardtype, cardnumber);
+            handCardsArray.push(sign[r]);
+            sign.splice(r, 1);
 
-        }
-        function cardgenerate(_cardtype: string, _cardnumber: string): void {
-            let div: HTMLElement = document.createElement("div");
-            document.getElementById("Handkarten").appendChild(div);
-            if (_cardtype == "Pi" || _cardtype == "Kr") {
-                console.log(_cardtype + " if");
-                div.classList.add("schwarz");
-
-            } else {
-            div.classList.add("rot"); }
-
-            div.classList.add("Handkarten");
-
-            div.innerHTML = "<p>" + _cardnumber + "</p>";
         }
 
 
     }
-    document.addEventListener("DOMContentLoaded", Eingabe);
+
+
+
+
+
+
+    function cardgenerate(_cardtype: string, _cardnumber: string): void {
+        let div: HTMLElement = document.createElement("div");
+        document.getElementById("Handkarten").appendChild(div);
+        if (_cardtype == "Pi" || _cardtype == "Kr") {
+            console.log(_cardtype + " if");
+            div.classList.add("schwarz");
+
+        } else {
+            div.classList.add("rot");
+        }
+
+        div.classList.add("Handkarten");
+
+        div.innerHTML = "<p>" + cardshow(_cardtype) + _cardnumber + "</p>";
+    }
+    function cardshow(_cardtype: string): string {
+        let emblem: string;
+        switch (_cardtype) {
+            case "Kr": {
+                emblem = "♣";
+                break;
+            }
+            case "Pi": {
+                emblem = "♠";
+                break;
+            }
+            case "He": {
+                emblem = "♥";
+                break;
+            }
+            case "Ka": {
+                emblem = "♦";
+                break;
+            }
+            default: {
+
+                break;
+            }
+
+        }
+        return emblem;
+    }
+
 }
+
+
+
