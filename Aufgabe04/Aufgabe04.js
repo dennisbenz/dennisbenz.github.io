71fb83cc9d3f206110a97ede86375390f026eaf2
dennisbenz <dennis.vimon@web.de>
2019-04-25 13:26:07 +0200

    aufgabe 04 verbessert

--- Aufgabe04/Aufgabe04.js
+++ Aufgabe04/Aufgabe04.js
@@ -1,32 +1,63 @@
-var Aufgabe04;
-(function (Aufgabe04) {
-    document.addEventListener("DOMContentLoaded", init);
-    let fieldset = document.getElementsByTagName("fieldset")[2].getElementsByTagName("input").length;
-    console.log(fieldset);
-})(Aufgabe04 || (Aufgabe04 = {}));
-function init(_event) {
-    console.log("Init");
-    let fieldsets = document.getElementsByTagName("fieldset");
-    for (let i = 0; i < fieldsets.length; i++) {
-        let fieldset = fieldsets[i];
-        fieldset.addEventListener("change", handleChange);
+var task4;
+(function (task4) {
+    //Eventlistener
+    document.addEventListener("DOMContentLoaded", showPrice);
+    function showPrice(_event) {
+        document.getElementById("checkButton").addEventListener("click", pruefungDerDaten);
+        let fieldsets = document.getElementsByTagName("fieldset");
+        for (let i = 0; i < fieldsets.length; i++) {
+            let fieldset = fieldsets[i];
+            fieldset.addEventListener("change", preisBerechnen);
+        }
     }
-}
-function handleChange(_event) {
-    console.log(_event);
-    //*
-    let target = _event.target;
-    console.log("Changed " + target.name + " to " + target.value);
-    //*/
-    //*/ note: this == _event.currentTarget in an event-handler
-    if (this.id == "number")
-        console.log("Changed " + target.name + " to " + target.checked);
-    //*/
-    //*
-    if (target.name == "Stepper") {
-        let meter = document.getElementsByTagName("check3")[0];
-        meter.value = parseFloat(target.value);
+    //Preisberechnung und Erstellung von p Tag im Checkout
+    function preisBerechnen(_event) {
+        let n = 0;
+        document.getElementById("Order").innerHTML = "";
+        let input = document.getElementsByTagName("input");
+        for (let i = 0; i < input.length; i++) {
+            if (input[i].checked == true) {
+                let preis = Number(input[i].value);
+                n += preis;
+                let orderName = document.createElement("li");
+                orderName.innerHTML = `<p>${input[i].className}</p>`;
+                document.getElementById("Order").appendChild(orderName);
+            }
+            if (input[i].name == "Stepper") {
+                let stepperNumber = Number(input[i].value);
+                n += stepperNumber; /* * preisStepper; */
+                if (stepperNumber > 0) {
+                    let orderName = document.createElement("li");
+                    orderName.innerHTML = `<p>${stepperNumber} ${input[i].className}</p>`;
+                    document.getElementById("Order").appendChild(orderName);
+                }
+            }
+        }
+        document.getElementById("showprice").innerHTML = n.toFixed(2).toString();
     }
-    //*/
-}
+    function pruefungDerDaten() {
+        let leereFormulare = [];
+        let pruefung = document.getElementsByTagName("input");
+        for (let i = 0; i < pruefung.length; i++) {
+            if (pruefung[i].value == "") {
+                let feldName = pruefung[i].name;
+                leereFormulare.push(feldName);
+            }
+            if (pruefung[i].value == "") {
+                let orderName = pruefung[i].name;
+                leereFormulare.push(orderName);
+            }
+            if (pruefung[i].checked == false) {
+                let feldClass = pruefung[i].className;
+                leereFormulare.push(feldClass);
+            }
+        }
+        if (leereFormulare.length == 0) {
+            alert("Ihre Bestellung wurde erfolgreich abgesendet! Vielen Dank:)");
+        }
+        else {
+            alert(`${leereFormulare} fehlt noch`);
+        }
+    }
+})(task4 || (task4 = {}));
 //# sourceMappingURL=Aufgabe04.js.map
--- Aufgabe04/Aufgabe04.ts
+++ Aufgabe04/Aufgabe04.ts
@@ -1,45 +1,76 @@
-namespace Aufgabe04 {
-    document.addEventListener("DOMContentLoaded", init);
-    let fieldset: number = document.getElementsByTagName("fieldset")[2].getElementsByTagName("input").length;
-    console.log(fieldset);
-}
+namespace task4 {
 
+    //Eventlistener
+    document.addEventListener("DOMContentLoaded", showPrice);
 
+    function showPrice(_event: Event): void {
+        document.getElementById("checkButton").addEventListener("click", pruefungDerDaten);
+        let fieldsets: HTMLCollectionOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
 
-
-
-function init(_event: Event): void {
-    console.log("Init");
-    let fieldsets: HTMLCollectionOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
-
-    for (let i: number = 0; i < fieldsets.length; i++) {
-        let fieldset: HTMLFieldSetElement = fieldsets[i];
-        fieldset.addEventListener("change", handleChange);
-    }
-}
-
-
-
-
-function handleChange(_event: Event): void {
-    console.log(_event);
-    //*
-    let target: HTMLInputElement = <HTMLInputElement>_event.target;
-    console.log("Changed " + target.name + " to " + target.value);
-    //*/
-    //*/ note: this == _event.currentTarget in an event-handler
-    if (this.id == "number")
-        console.log("Changed " + target.name + " to " + target.checked);
-    //*/
-
-    //*
-    if (target.name == "Stepper") {
-        let meter: HTMLMeterElement = <HTMLMeterElement>document.getElementsByTagName("check3")[0];
-        meter.value = parseFloat(target.value);
-
+        for (let i: number = 0; i < fieldsets.length; i++) {
+            let fieldset: HTMLFieldSetElement = fieldsets[i];
+            fieldset.addEventListener("change", preisBerechnen);
+        }
     }
 
-    
 
-    //*/
+    //Preisberechnung und Erstellung von p Tag im Checkout
+    function preisBerechnen(_event: Event): void {
+        let n: number = 0;
+        document.getElementById("Order").innerHTML = "";
+        let input: HTMLCollectionOf<HTMLInputElement> = document.getElementsByTagName("input");
+        for (let i: number = 0; i < input.length; i++) {
+            if (input[i].checked == true) {
+                let preis: number = Number(input[i].value);
+                n += preis;
+                let orderName: HTMLLIElement = document.createElement("li");
+                orderName.innerHTML = `<p>${input[i].className}</p>`;
+                document.getElementById("Order").appendChild(orderName);
+
+            }
+            if (input[i].name == "Stepper") {
+                let stepperNumber: number = Number(input[i].value);
+
+                n += stepperNumber; /* * preisStepper; */
+                if (stepperNumber > 0) {
+                    let orderName: HTMLLIElement = document.createElement("li");
+                    orderName.innerHTML = `<p>${stepperNumber} ${input[i].className}</p>`;
+                    document.getElementById("Order").appendChild(orderName);
+                }
+            }
+        }
+
+        document.getElementById("showprice").innerHTML = n.toFixed(2).toString();
+    }
+
+
+
+    function pruefungDerDaten(): void {
+        let leereFormulare: string[] = [];
+        let pruefung: HTMLCollectionOf<HTMLInputElement> = document.getElementsByTagName("input");
+        for (let i: number = 0; i < pruefung.length; i++) {
+            if (pruefung[i].value == "") {
+                let feldName: string = pruefung[i].name;
+                leereFormulare.push(feldName);
+            }
+            if (pruefung[i].value == "") {
+                let orderName: string = pruefung[i].name;
+                leereFormulare.push(orderName);
+            }
+
+
+
+
+            if (pruefung[i].checked == false) {
+                let feldClass: string = pruefung[i].className;
+                leereFormulare.push(feldClass);
+            }
+        }
+        if (leereFormulare.length == 0) {
+            alert("Ihre Bestellung wurde erfolgreich abgesendet! Vielen Dank:)");
+        }
+        else { alert(`${leereFormulare} fehlt noch`); }
+    }
+
+
 }
