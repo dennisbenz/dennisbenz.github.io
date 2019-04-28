c1e9d963723eb050a966ad2f67e51eb7ac4fd5a1
dennisbenz <dennis.vimon@web.de>
2019-04-28 17:58:38 +0200

    aufgabe05

---
+++ Aufgabe05/data.ts
@@ -0,0 +1,61 @@
+namespace task5 {
+    export interface HeteroPredefined {
+        type: string;
+        name: string;
+        price: number;
+        value: number;
+        id: string;
+        class: string;
+        minimum: number;
+        maximum: number;
+    }
+
+    export interface HomogenousArray {
+        [array: string]: HeteroPredefined[];
+    }
+
+    export let product: HomogenousArray = {
+        "Wähle deine Anrichtung:": [
+            { type: "radio", name: "waffelbecher", price: 0.50, value: 0, id: "radio1", class: "Waffel", minimum: 0, maximum: 0 },
+            { type: "radio", name: "waffelbecher", price: 0.00, value: 0, id: "radio2", class: "Becher", minimum: 0, maximum: 0 }
+        ],
+
+
+        "Icecreams:": [
+            { type: "number", name: "Erdbeere", price: 1, value: 0, id: "Erdbeere", class: "Erdbeere", minimum: 0, maximum: 6 },
+            { type: "number", name: "Nutella", price: 1, value: 0, id: "Nutelle", class: "Nutella", minimum: 0, maximum: 6 },
+            { type: "number", name: "Schokolade", price: 1, value: 0, id: "Schokolade", class: "Schokolade", minimum: 0, maximum: 6 },
+            { type: "number", name: "Himbeer", price: 1, value: 0, id: "Himbeer", class: "Himbeer", minimum: 0, maximum: 6 },
+            { type: "number", name: "Apfel", price: 1, value: 0, id: "Apfel", class: "Apfel", minimum: 0, maximum: 6 },
+            { type: "number", name: "Mango", price: 1, value: 0, id: "Mango", class: "Mango", minimum: 0, maximum: 6 },
+            { type: "number", name: "Melone", price: 1, value: 0, id: "Melone", class: "Melone", minimum: 0, maximum: 6 },
+            { type: "number", name: "Vanille", price: 1, value: 0, id: "Vanille", class: "Vanille", minimum: 0, maximum: 6 },
+            { type: "number", name: "Cookies", price: 1, value: 0, id: "Cookies", class: "Cookies", minimum: 0, maximum: 6 },
+            { type: "number", name: "Pfirsich", price: 1, value: 0, id: "Pfirsich", class: "Pfirsich", minimum: 0, maximum: 6 },
+            { type: "number", name: "Zitrone", price: 1, value: 0, id: "Zitrone", class: "Zitrone", minimum: 0, maximum: 6 },
+            { type: "number", name: "Kirsche", price: 1, value: 0, id: "Kirsche", class: "Kirsche", minimum: 0, maximum: 6 },
+            { type: "number", name: "Amarena", price: 1, value: 0, id: "Amarena", class: "Amarena", minimum: 0, maximum: 6 },
+            { type: "number", name: "Kiwi", price: 1, value: 0, id: "Kiwi", class: "Kiwi", minimum: 0, maximum: 6 },
+            { type: "number", name: "Kokos", price: 1, value: 0, id: "Kokos", class: "Kokos", minimum: 0, maximum: 6 },
+            { type: "number", name: "Orange", price: 1, value: 0, id: "Orange", class: "Orange", minimum: 0, maximum: 6 }
+
+        ],
+        "Extras:": [
+            { type: "checkbox", name: "Krokant", price: 0.50, value: 0, id: "check1", class: "Krokant", minimum: 0, maximum: 0 },
+            { type: "checkbox", name: "Schokosoße", price: 0.50, value: 0, id: "check2", class: "Schokosoße", minimum: 0, maximum: 0 },
+            { type: "checkbox", name: "Erdbeersoße", price: 0.50, value: 0, id: "check3", class: "Erdbeersoße", minimum: 0, maximum: 0 },
+            { type: "checkbox", name: "Sahne", price: 0.50, value: 0, id: "check4", class: "Sahne", minimum: 0, maximum: 0 }
+        ]
+    };
+
+    /* = {
+    "sorten": [
+        {type:"number", name:"Stepper", value: 1, id:"check1", class:"1 Kugel Schoko"},
+        {type:"number", name:"Stepper", value:1, id:"check2", class:"1 Kugel Vanille"},
+        {type:"number", name:"Stepper", value:1.2, id:"check3", class:"1 Kugel Zitrone"},
+        {type:"number", name:"Stepper", value:1.2, id:"check4", class:"1 Kugel Himbeer"},
+        {type:"number", name:"Stepper", value:1.5, id:"check5", class:"1 Kugel Nuss"}
+    ],
+    }; */
+
+}
